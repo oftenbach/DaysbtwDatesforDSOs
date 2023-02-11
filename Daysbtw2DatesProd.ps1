5cb1874236297223c841058357a8673a6f815c47
Torrey W <50078854+oftenbach@users.noreply.github.com>
2023-02-11 12:05:10 -0600

    User interface updates

--- Daysbtw2DatesProd.ps1
+++ Daysbtw2DatesProd.ps1
@@ -1,29 +1,32 @@
-Write-Host "This shell script will ask you to enter two dates and automatically calculate the number of days between them for you."
-Write-Host "You may repeat calculations ad infinitum so long as you answer 'y', 'Y', 'yes', 'Yes', or 'YES' when asked whether you wish to repeat the operation."
-Write-Host "Once 'n', 'N', 'no', 'No', or 'NO' is received as a response, you may add the calculated total of all prior calculations."
+<#To run this script on macOS, download then drag and drop this file into an open PowerShell window and hit 'enter' on your keyboard.#>
+
+Write-Host "This script prompts you to enter 2 dates and automatically calculates the number of days between them (inclusive of the end date)."
+Write-Host "You may repeat calculations ad infinitum so long as you enter 'y', 'Y', 'yes', 'Yes', or 'YES' when asked 'Do you wish to repeat the operation?'"
+Write-Host "When 'n', 'N', 'no', 'No', or 'NO' is entered instead, the script asks if you wish to have your prior calculations totaled."
 Write-Host "Sample Use Cases:"
-Write-Host "(i) Determine how many days a student's FT CPT authorization entails to enter in your 'CPT Recommended' template email."
-Write-Host "(ii) Determine if student has exceeded or will exceed maximum number of total FT CPT days during processing of a CPT or OPT authorization request."
+Write-Host "(i) Determine how many days a student's FT CPT (Full-Time Curricular Practical Training) authorization entails to enter in your 'CPT Recommended' template email."
+Write-Host "(ii) Determine if student has exceeded or will exceed, respectively, maximum number of total FT CPT days permitted without losing all OPT eligibility at current degree level during processing of an OPT or CPT request."
 
 $totalSum = 0
-$startdate = Read-Host "Please enter a start date in the format MM/DD/YYYY"
-$enddate = Read-Host "Please enter an end date in the format MM/DD/YYYY"
+Write-Host "--------CALCULATE # OF DAYS BTW ANY DATE RANGE---------"
+$startdate = Read-Host "Please enter a start date in the format M/D/YYYY"
+$enddate = Read-Host "Please enter an end date in the format M/D/YYYY"
 $totalnum = (New-TimeSpan -Start $startdate -End $enddate).Days + 1
 Write-Host "The number of days between the provided dates is: $totalnum"
 $totalSum +=$totalnum
-$repeat = Read-Host "Do you wish to repeat this operation? (Y/N)"
+$repeat = Read-Host "Do you wish to repeat this operation for a new date range? (Y/N)"
 while ($repeat -eq 'Y' -or $repeat -eq 'y' -or $repeat -eq 'Yes' -or $repeat -eq 'YES' -or $repeat -eq 'yes') { 
-    $startdate = Read-Host "Please enter a start date in the format MM/DD/YYYY" 
-    $enddate = Read-Host "Please enter an end date in the format MM/DD/YYYY" 
+    $startdate = Read-Host "Please enter a start date in the format M/D/YYYY" 
+    $enddate = Read-Host "Please enter an end date in the format M/D/YYYY" 
     $totalnum = (New-TimeSpan -Start $startdate -End $enddate).Days + 1 
     Write-Host "The number of days between the provided dates is: $totalnum" 
     $totalSum +=$totalnum
-    $repeat = Read-Host "Do you wish to repeat this operation? (Y/N)" }
+    $repeat = Read-Host "Do you wish to repeat this operation for a new date range? (Y/N)" }
 if ($repeat -eq 'N' -or $repeat -eq 'n' -or $repeat -eq 'No' -or $repeat -eq 'NO' -or $repeat -eq 'no') {
-    $total = Read-Host "Do you wish to total your previous calculations?" 
+    $total = Read-Host "Do you wish to total the previous calculations?" 
     if ($total -eq 'y' -or $total -eq 'Y' -or $total -eq 'Yes' -or $total -eq 'YES' -or $total -eq 'yes') { 
     Write-Host "The sum of all previous calculations is: $totalSum"   
-    $answer = Read-Host "Do you want to restart this program (Y/N)?"
+    $answer = Read-Host "Do you wish to start a new session of this program for another student/case (Y) or exit now (N)?"
     if ($answer -eq "y" -or $answer -eq 'Y' -or $answer -eq 'Yes' -or $answer -eq 'YES' -or $answer -eq 'yes') {
     & $MyInvocation.MyCommand
     } }}
