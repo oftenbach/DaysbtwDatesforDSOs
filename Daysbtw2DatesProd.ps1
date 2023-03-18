6401963cd5c3b175cf82aa2802c62d0ea280892f
Torrey W <50078854+oftenbach@users.noreply.github.com>
2023-03-18 10:07:44 -0500

    Provided option to start new session after consecutive 'no's received in answer to question whether user wants to calculate a new date range and if they'd like to start a new session, respectively.

--- Daysbtw2DatesProd.ps1
+++ Daysbtw2DatesProd.ps1
@@ -1,34 +1,47 @@
+Write-Host "`n"
+Write-Host ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>DAYS BETWEEN DATES: A TOOL FOR DSOs<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+Write-Host "---------------------------------GENERAL USAGE---------------------------------"
 Write-Host "This script prompts you to enter 2 dates and automatically calculates the number of days between them (inclusive of the end date)."
 Write-Host "You may repeat calculations ad infinitum so long as you enter 'y', 'Y', 'yes', 'Yes', 'YES', etc. when asked 'Do you wish to repeat this operation for a new date range? (Y/N)'"
+Write-Host "`n"
 Write-Host "When 'n', 'N', 'no', 'No', 'NO', etc. is entered instead, the script asks if you wish to have your previous calculations for the current session totaled." 
-Write-Host "If 'yes' (or one of the aforementioned semantic equivalents) is entered, the total of all calculations obtained in the current session is given, after which the option to start a new session of calculations or exit the program displays; any other input here force-quits this shell window instantly."
-Write-Host "Sample Use Cases:"
-Write-Host "(i) Track and relay to a student how many days the FT CPT (Full-Time Curricular Practical Training) request which you've just authorized entails in your office's tailored CPT approval email."
-Write-Host "(ii) Determine if a student has exceeded or will exceed, respectively, the maximum number of total FT CPT days permitted without losing OPT eligibility at current degree level while processing their OPT or CPT request."
-
+Write-Host "`n"
+Write-Host "If 'yes' (or one of the aforementioned semantic equivalents) is entered, the total of all calculations obtained in the current session is given, after which the option to start a new session of calculations or exit the program displays; if 'no' et al. is entered, the same option is presented to start a new session; any other input here immediately terminates the shell window."
+Write-Host "`n"
+Write-Host "---------------------------------SAMPLE USE CASES---------------------------------"
+Write-Host "(i) Track and relay to a student how many days the FT CPT (Full-Time Curricular Practical Training) request which you've authorized entails in your office's tailored CPT approval email."
+Write-Host "(ii) Determine if a student has exceeded or will exceed the maximum number of total FT CPT days permitted without losing OPT eligibility at current degree level while processing their OPT or CPT request."
+Write-Host "(iii) Calculate how many days of medical RCL/withdrawal a student has accrued."
+Write-Host "`n"
 $totalSum = 0
-Write-Host "--------CALCULATE # OF DAYS BTW ANY DATE RANGE---------"
+Write-Host "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<START DAYS BETWEEN DATES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 $startdate = Read-Host "Please enter a start date in the format M/D/YYYY"
 $enddate = Read-Host "Please enter an end date in the format M/D/YYYY"
 $totalnum = (New-TimeSpan -Start $startdate -End $enddate).Days + 1
 Write-Host "The number of days between the provided dates is: $totalnum"
 $totalSum +=$totalnum
 $repeat = Read-Host "Do you wish to repeat this operation for a new date range? (Y/N)"
-while ($repeat -eq 'Y' -or $repeat -eq 'y' -or $repeat -eq 'Yes' -or $repeat -eq 'YES' -or $repeat -eq 'yes') { 
+while ($repeat -eq 'Y' -or $repeat -eq 'y' -or $repeat -eq 'Yes' -or $repeat -eq 'YES' -or $repeat -eq 'yes' -or $repeat -eq 'yEs' -or $repeat -eq 'yES' -or $repeat -eq 'YeS') { 
     $startdate = Read-Host "Please enter a start date in the format M/D/YYYY" 
     $enddate = Read-Host "Please enter an end date in the format M/D/YYYY" 
     $totalnum = (New-TimeSpan -Start $startdate -End $enddate).Days + 1 
     Write-Host "The number of days between the provided dates is: $totalnum" 
     $totalSum +=$totalnum
     $repeat = Read-Host "Do you wish to repeat this operation for a new date range? (Y/N)" }
-if ($repeat -eq 'N' -or $repeat -eq 'n' -or $repeat -eq 'No' -or $repeat -eq 'NO' -or $repeat -eq 'no') {
+if ($repeat -eq 'N' -or $repeat -eq 'n' -or $repeat -eq 'No' -or $repeat -eq 'NO' -or $repeat -eq 'no' -or $repeat -eq 'nO') {
     $total = Read-Host "Do you wish to total the previous calculations?" 
-    if ($total -eq 'y' -or $total -eq 'Y' -or $total -eq 'Yes' -or $total -eq 'YES' -or $total -eq 'yes') { 
+    if ($total -eq 'y' -or $total -eq 'Y' -or $total -eq 'Yes' -or $total -eq 'YES' -or $total -eq 'yes' -or $total -eq 'yEs' -or $total -eq 'yES' -or $total -eq 'YeS') { 
     Write-Host "The sum of all previous calculations is: $totalSum"   
     $answer = Read-Host "Do you wish to start a new session of this program for another student/case (Y) or exit now (N)?"
-    if ($answer -eq "y" -or $answer -eq 'Y' -or $answer -eq 'Yes' -or $answer -eq 'YES' -or $answer -eq 'yes') {
+    if ($answer -eq "y" -or $answer -eq 'Y' -or $answer -eq 'Yes' -or $answer -eq 'YES' -or $answer -eq 'yes' -or $answer -eq 'yES' -or $answer -eq 'yEs' -or $answer -eq 'YeS') {
     & $MyInvocation.MyCommand
-    } }}
+    } }
+    if ($total -eq 'n' -or $total -eq 'N' -or $total -eq 'No' -or $total -eq 'NO' -or $total -eq 'no' -or $total -eq 'nO') { 
+    $answer = Read-Host "Do you wish to start a new session of this program for another student/case (Y) or exit now (N)?"
+    if ($answer -eq "y" -or $answer -eq 'Y' -or $answer -eq 'Yes' -or $answer -eq 'YES' -or $answer -eq 'yes' -or $answer -eq 'yES' -or $answer -eq 'yEs' -or $answer -eq 'YeS') {
+        & $MyInvocation.MyCommand
+        } }
+    }
 else {
     exit
 }
--- Daysbtw2DatesProd.py
+++ Daysbtw2DatesProd.py
@@ -1,15 +1,18 @@
+print("\n")
+print("-------------------------------ABOUT DAYS BETWEEN DATES CALCULATOR-------------------------------")
 print("This script prompts you to enter 2 dates and automatically calculates the number of days between them (inclusive of the end date).")
-print("You may repeat calculations ad infinitum so long as you enter 'y', 'Y', 'yes', 'Yes', or 'YES' when asked 'Do you wish to repeat this operation for a new date range? (Y/N)'")
-print("When 'n', 'N', 'no', 'No', or 'NO' is entered instead, the script asks if you wish to have your previous calculations for the current session totaled.")
-print("If 'yes' (or one of the aforementioned semantic equivalents) is entered, the total of all calculations obtained in the current session is given, after which the option to start a new session of calculations or exit the program displays; any other input here force-quits this shell window instantly.")
-print("Sample Use Cases:")
+print("You may repeat calculations ad infinitum so long as you enter 'y', 'Y', 'yes', 'Yes', 'YES', etc., when asked 'Do you wish to repeat this operation for a new date range? (Y/N)'")
+print("When 'n', 'N', 'no', 'No', or 'NO', etc., is entered instead, the script asks if you wish to have your previous calculations for the current session totaled.")
+print("If 'yes' (or one of the aforementioned semantic equivalents) is entered, the total of all calculations obtained in the current session is given, after which the option to start a new session of calculations or exit the program displays; if 'no' et al., the option to start a new session is also presented. However, any other input here instantly terminates this shell window.")
+print("-------------------------------SAMPLE USE CASES-------------------------------")
 print("(i) Track and relay to a student how many days the FT CPT (Full-Time Curricular Practical Training) request which you've just authorized entails in your office's tailored CPT approval email.")
 print("(ii) Determine if a student has exceeded or will exceed, respectively, the maximum number of total FT CPT days permitted without losing OPT eligibility at current degree level while processing their OPT or CPT request.")
+print("(iii) Determine how many days of medical RCL a student has accrued.")
 
 import datetime
 
 total_sum = 0
-print("--------CALCULATE # OF DAYS BTW ANY DATE RANGE---------")
+print("-------------------------------DAYS BETWEEN DATES: A TOOL FOR DSOs-------------------------------")
 start_date = input("Please enter a start date in the format m/d/yyyy: ") 
 #convert start date to datetime object 
 start_date = datetime.datetime.strptime(start_date, "%m/%d/%Y") 
@@ -20,7 +23,7 @@ difference = (end_date - start_date).days + 1
 print("The number of days between your two dates is", difference)
 total_sum += difference
 repeat = input("Do you wish to repeat this operation for a new date range? (Y/N): ").lower()
-while repeat == 'y' or repeat == 'Y' or repeat == 'YES' or repeat == 'yes' or repeat == 'Yes':
+while repeat == 'y' or repeat == 'yes' or repeat == 'Y' or repeat == 'Yes' or repeat == 'YES' or repeat == 'YeS' or repeat == 'yES' or repeat == 'yEs':
     start_date = input("Please enter a start date in the format m/d/yyyy: ") 
     start_date = datetime.datetime.strptime(start_date, "%m/%d/%Y") 
     end_date = input("Please enter an end date in the format m/d/yyyy: ") 
@@ -29,10 +32,11 @@ while repeat == 'y' or repeat == 'Y' or repeat == 'YES' or repeat == 'yes' or re
     print("The number of days between the provided dates is:", difference)
     total_sum += difference
     repeat = input("Do you wish to repeat this operation for a new date range? (Y/N): ").lower()
-if repeat == 'n' or repeat == 'no' or repeat == 'N' or repeat == 'NO' or repeat == 'No':
+if repeat == 'n' or repeat == 'no' or repeat == 'N' or repeat == 'NO' or repeat == 'NO' or repeat == 'nO' or repeat == 'No':
     total = input("Do you wish to total the previous calculations? (Y/N): ").lower()
-    if total == 'y' or total == 'yes' or total == 'Y' or total == 'Yes' or total == 'YES':
+    if total == 'y' or total == 'yes' or total == 'Y' or total == 'Yes' or total == 'YES' or total == 'YeS' or total == 'yES' or total == 'yEs':
         print("The sum of all previous calculations is:", total_sum)
+    if total == 'n' or total == 'no' or total == 'N' or total == 'NO' or total == 'NO' or total == 'nO' or total == 'No':
         answer = input("Do you wish to start a new session of this program for another student/case (Y) or exit now (N)?: ").lower()
         if answer == 'y' or answer == 'yes' or answer == 'Y' or answer == 'Yes' or answer == 'YES':
             start_date = input("Please enter a start date in the format M/D/YYYY: ")
